47e221b24e36b16df102795625cd74efd6f812f3
Tamas Piros <me@tamas.io>
2015-01-31 23:55:41 +0100

    search update

--- public/js/search/textsearch/textsearch.controller.js
+++ public/js/search/textsearch/textsearch.controller.js
@@ -8,14 +8,19 @@
 
   function textSearchController(photofactory) {
     var vm = this;
+    vm.results = [];
     vm.search = function() {
-      photofactory.textSearch(vm.term)
-      .then(function(data) {
-        vm.results = data;
-      })
-      .catch(function(error) {
-        console.error(error);
-      });
-    }
+      if (vm.term || vm.term.length !== 0) {
+        photofactory.textSearch(vm.term)
+        .then(function(data) {
+          vm.results = data;
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
+      } else {
+        vm.results = [];
+      }
+    };
   }
 })();
