ba94c7de950c22699b27a42bf23df0da96c7014b
Dave Cassel <dmcassel@gmail.com>
2015-05-27 16:04:09 -0400

    Check whether the application server already exists. If so, skip app server and database creation

--- ml-setup/setup.js
+++ ml-setup/setup.js
@@ -3,6 +3,7 @@ var request = require('request');
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require("fs"));
 var path = require('path');
+var prequest = Promise.promisify(require("request"));
 
 var username="admin"; // update if required
 var password="admin"; // update if required
@@ -44,22 +45,49 @@ function applyConfig(path, method, config) {
   });
 }
 
+// Check whether the REST API application server has already been made
+function checkForAppServer(restConfig) {
+  return prequest
+    ({
+      url: 'http://localhost:8002/v1/rest-apis/' + restConfig['rest-api'].name,
+      auth: getAuth()
+    });
+}
+
+// If the app server does not already exist, create it and the databases that
+// go with it. The response parameter is the response to a request to get
+// information about the app server.
+function createIfNeeded(response) {
+  if (response[0].statusCode === 404) {
+    // the Application Server has not already been set up
+    console.log('Setting up the application server and the databases - ');
+    return applyConfig('/v1/rest-apis', 'POST', restConfig.value());
+  } else {
+    console.log('App server already setup; skipping');
+  }
+}
+
 // Create the application server and the content and modules databases. 
 function bootstrap() {
-  readFile('01-rest-instance-config.json')
-    .then(
-      function (restConfig) {
-        console.log('Setting up the application server and the databases - ');
-        return applyConfig('/v1/rest-apis', 'POST', JSON.parse(restConfig));
-      })
+  var restConfig =
+    readFile('01-rest-instance-config.json')
+      .then(JSON.parse);
+
+  restConfig.then(checkForAppServer)
+    .then(createIfNeeded)
     .then(function() { return readFile('02-database-config.json'); })
+    .then(JSON.parse)
     .then(function(dbConfig) {
       console.log('Setting up the indexes - ');
-      return applyConfig('/manage/v2/databases/geophoto-content/properties', 'PUT', JSON.parse(dbConfig));
+      return applyConfig('/manage/v2/databases/geophoto-content/properties', 'PUT', dbConfig);
     })
     .catch(
       function (error) {
-        console.log('Problem: ' + error.errorResponse.statusCode + '; ' + error.errorResponse.message);
+        if (typeof error === 'object') {
+          console.log('Problem: ' + error.errorResponse.statusCode + '; ' + error.errorResponse.message);
+        } else {
+          console.log('Problem: ' + error);
+        }
       });
 }
 
